b3d2ec36ea99ebcf134a1118fb5f9b36abd419ea
skysys <ncepucs1807@163.com>
2023-02-26 21:30:50 +0800

    add backend

---
+++ backend/app/api/api_test.py
@@ -0,0 +1,215 @@
+import logging
+import random
+import uuid
+import os
+from flask import Blueprint, jsonify, session, request, current_app
+from datetime import datetime, timedelta
+from decimal import Decimal
+
+from app.api.tree import Tree
+from app.utils.code import ResponseCode
+from app.utils.response import ResMsg
+from app.utils.util import route, Redis, CaptchaTool, PhoneTool
+from app.utils.auth import Auth, login_required
+from app.api.report import excel_write, word_write, pdf_write
+from app.api.wx_login_or_register import get_access_code, get_wx_user_info, wx_login_or_register
+from app.api.phone_login_or_register import SendSms, phone_login_or_register
+from app.celery import add, flask_app_context
+
+bp = Blueprint("test", __name__, url_prefix='/')
+
+logger = logging.getLogger(__name__)
+
+
+# -----------------原生蓝图路由---------------#
+
+
+@bp.route('/logs', methods=["GET"])
+def test_logger():
+    """
+    测试自定义logger
+    :return:
+    """
+    logger.info("this is info")
+    logger.debug("this is debug")
+    logger.warning("this is warning")
+    logger.error("this is error")
+    logger.critical("this is critical")
+    return "ok"
+
+
+@bp.route("/unifiedResponse", methods=["GET"])
+def test_unified_response():
+    """
+    测试统一返回消息
+    :return:
+    """
+    res = ResMsg()
+    test_dict = dict(name="zhang", age=18)
+    res.update(code=ResponseCode.Success, data=test_dict)
+    return jsonify(res.data)
+
+
+# --------------使用自定义封装蓝图路由--------------------#
+
+
+@route(bp, '/packedResponse', methods=["GET"])
+def test_packed_response():
+    """
+    测试响应封装
+    :return:
+    """
+    res = ResMsg()
+    test_dict = dict(name="zhang", age=18)
+    # 此处只需要填入响应状态码,即可获取到对应的响应消息
+    res.update(code=ResponseCode.Success, data=test_dict)
+    # 此处不再需要用jsonify，如果需要定制返回头或者http响应如下所示
+    # return res.data,200,{"token":"111"}
+    return res.data
+
+
+@route(bp, '/typeResponse', methods=["GET"])
+def test_type_response():
+    """
+    测试返回不同的类型
+    :return:
+    """
+    res = ResMsg()
+    now = datetime.now()
+    date = datetime.now().date()
+    num = Decimal(11.11)
+    test_dict = dict(now=now, date=date, num=num)
+    # 此处只需要填入响应状态码,即可获取到对应的响应消息
+    res.update(code=ResponseCode.Success, data=test_dict)
+    # 此处不再需要用jsonify，如果需要定制返回头或者http响应如下所示
+    # return res.data,200,{"token":"111"}
+    return res.data
+
+
+# --------------Redis测试封装--------------------#
+
+@route(bp, '/testRedisWrite', methods=['GET'])
+def test_redis_write():
+    """
+    测试redis写入
+    """
+    # 写入
+    Redis.write("test_key", "test_value", 60)
+    return "ok"
+
+
+@route(bp, '/testRedisRead', methods=['GET'])
+def test_redis_read():
+    """
+    测试redis获取
+    """
+    data = Redis.read("test_key")
+    return data
+
+
+# -----------------图形验证码测试---------------------------#
+
+@route(bp, '/testGetCaptcha', methods=["GET"])
+def test_get_captcha():
+    """
+    获取图形验证码
+    :return:
+    """
+    res = ResMsg()
+    new_captcha = CaptchaTool()
+    img, code = new_captcha.get_verify_code()
+    res.update(data=img)
+    session["code"] = code
+    return res.data
+
+
+@route(bp, '/testVerifyCaptcha', methods=["POST"])
+def test_verify_captcha():
+    """
+    验证图形验证码
+    :return:
+    """
+    res = ResMsg()
+    obj = request.get_json(force=True)
+    code = obj.get('code', None)
+    s_code = session.get("code", None)
+    print(code, s_code)
+    if not all([code, s_code]):
+        res.update(code=ResponseCode.InvalidParameter)
+        return res.data
+    if code != s_code:
+        res.update(code=ResponseCode.VerificationCodeError)
+        return res.data
+    return res.data
+
+
+# --------------------JWT测试-----------------------------------------#
+
+@route(bp, '/testLogin', methods=["POST"])
+def test_login():
+    """
+    登陆成功获取到数据获取token和刷新token
+    :return:
+    """
+    res = ResMsg()
+    obj = request.get_json(force=True)
+    user_name = obj.get("name")
+    # 未获取到参数或参数不存在
+    if not obj or not user_name:
+        res.update(code=ResponseCode.InvalidParameter)
+        return res.data
+
+    if user_name == "qin":
+        # 生成数据获取token和刷新token
+        access_token, refresh_token = Auth.encode_auth_token(user_id=user_name)
+
+        data = {"access_token": access_token.decode("utf-8"),
+                "refresh_token": refresh_token.decode("utf-8")
+                }
+        res.update(data=data)
+        return res.data
+    else:
+        res.update(code=ResponseCode.AccountOrPassWordErr)
+        return res.data
+
+
+@route(bp, '/testGetData', methods=["GET"])
+@login_required
+def test_get_data():
+    """
+    测试登陆保护下获取数据
+    :return:
+    """
+    res = ResMsg()
+    name = session.get("user_name")
+    data = "{}，你好！！".format(name)
+    res.update(data=data)
+    return res.data
+
+
+@route(bp, '/testRefreshToken', methods=["GET"])
+def test_refresh_token():
+    """
+    刷新token，获取新的数据获取token
+    :return:
+    """
+    res = ResMsg()
+    refresh_token = request.args.get("refresh_token")
+    if not refresh_token:
+        res.update(code=ResponseCode.InvalidParameter)
+        return res.data
+    payload = Auth.decode_auth_token(refresh_token)
+    # token被串改或过期
+    if not payload:
+        res.update(code=ResponseCode.PleaseSignIn)
+        return res.data
+
+    # 判断token正确性
+    if "user_id" not in payload:
+        res.update(code=ResponseCode.PleaseSignIn)
+        return res.data
+    # 获取新的token
+    access_token = Auth.generate_access_token(user_id=payload["user_id"])
+    data = {"access_token": access_token.decode("utf-8"), "refresh_token": refresh_token}
+    res.update(data=data)
+    return res.data
---
+++ backend/app/api/router.py
@@ -0,0 +1,6 @@
+from app.api.api_test import bp as bp_api_test
+from app.api.services import ArticleAPI
+
+router = [
+    bp_api_test,
+]
